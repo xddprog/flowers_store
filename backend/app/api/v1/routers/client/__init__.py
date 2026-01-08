291a1dd0e0da1b6bbc6ecc704f2a0fd4e19ad83d
magomedov marif <magomedovmarif2@gmail.com>
2026-01-08 20:04:46 +0300

    fix routes

--- backend/app/api/v1/routers/client/__init__.py
+++ backend/app/api/v1/routers/client/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
-
 from app.api.v1.routers.client.bouquet import router as bouquet_router
 from app.api.v1.routers.client.order import router as order_router
 from app.api.v1.routers.client.customer import router as customer_router
+from app.api.v1.routers.client.flower import router as flower_router
+
 
 api_v1_routers = APIRouter(prefix="/api/v1")
 api_v1_routers.include_router(bouquet_router, prefix="/bouquet", tags=["bouquet"])
 api_v1_routers.include_router(order_router, prefix="/order", tags=["order"])
 api_v1_routers.include_router(customer_router, prefix="/customer", tags=["customer"])
+api_v1_routers.include_router(flower_router, prefix="/flower", tags=["flower"])
